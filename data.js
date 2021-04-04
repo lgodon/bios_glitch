9eba82f6f9bd74215bf2ea4be835ed364f6dac8b
Leandro Godon <leandro.godon@gmail.com>
2021-04-04 15:37:58 -0300

    Assets del hall y ajustes de coordenadas.

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 //settings are just variables that can be sent to the client from the server
-//they are either related to the rooms or shared with the server 
+//they are either related to the rooms or shared with the server
 module.exports.SETTINGS = {
     //if not specified by the url where is the starting point
     defaultRoom: "frente",
@@ -53,10 +53,12 @@ module.exports.ROOMS = {
             h1a00fb: { cmd: "enter", room: "frente", label: "Afuera", point: [64, 96], enterPoint: [40, 90], obstacle: false },
         },
         things: {
-            mesita: { file: "mesitadete.png", frames: 2, frameDelay: 20, position: [56, 78], label: "Mesa de té", 
-                command: { cmd: "text", txt: "*Tienes un poco de frío*\n*Tomas un té calentito*", 
-                    align: "center", lines: 2, label: "", point: [60, 90] 
-                } 
+            flores: { file: "flores.png", frames: 1, frameDelay: 0, position: [19, 78], label: "" },
+            mesa: { file: "mesa.png", frames: 1, frameDelay: 0, position: [92, 86], label: "" },
+            mesita: { file: "mesitadete.png", frames: 2, frameDelay: 20, position: [56, 78], label: "Mesa de té",
+                command: { cmd: "text", txt: "*Tienes un poco de frío*\n*Tomas un té calentito*",
+                    align: "center", lines: 2, label: "", point: [60, 90]
+                }
             },
         }
     },
@@ -72,30 +74,30 @@ module.exports.ROOMS = {
             h8e24d9: { cmd: "enter", room: "hall", label: "Hall", point: [64, 96], enterPoint: [34, 75], obstacle: false },
         },
         things: {
-            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Inhalaciones territoriales", 
-                command: { cmd: "text", txt: "Inhalaciones territoriales\nde Ana Laura Cantera", 
-                    align: "center", lines: 2, url: "https://www.analauracantera.com.ar/inhalaciones", label: "", point: [25, 92] 
-                } 
+            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Inhalaciones territoriales",
+                command: { cmd: "text", txt: "Inhalaciones territoriales\nde Ana Laura Cantera",
+                    align: "center", lines: 2, url: "https://www.analauracantera.com.ar/inhalaciones", label: "", point: [25, 92]
+                }
             },
-            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Espacio de bioarte", 
-                command: { cmd: "text", txt: "Espacio de bioarte\nde Joaquín Fargas", 
-                    align: "center", lines: 2, url: "https://www.joaquinfargas.com/sala1/", label: "", point: [43, 80] 
-                } 
+            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Espacio de bioarte",
+                command: { cmd: "text", txt: "Espacio de bioarte\nde Joaquín Fargas",
+                    align: "center", lines: 2, url: "https://www.joaquinfargas.com/sala1/", label: "", point: [43, 80]
+                }
             },
-            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Cellular seeds", 
-                command: { cmd: "text", txt: "Cellular seeds de Alejandra Marinaro", 
-                    align: "center", lines: 2, url: "https://www.cellularseeds.com/", label: "", point: [61, 92] 
-                } 
+            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Cellular seeds",
+                command: { cmd: "text", txt: "Cellular seeds de Alejandra Marinaro",
+                    align: "center", lines: 2, url: "https://www.cellularseeds.com/", label: "", point: [61, 92]
+                }
             },
-            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "Agar Art", 
-                command: { cmd: "text", txt: "Agar Art", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/agarart#header", label: "", point: [79, 80] 
-                } 
+            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "Agar Art",
+                command: { cmd: "text", txt: "Agar Art",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/agarart#header", label: "", point: [79, 80]
+                }
             },
-            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Gabinete biológico", 
-                command: { cmd: "text", txt: "Gabinete biológico de Pablo Lapadula", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/pablo-lapadula#header", label: "", point: [97, 92] 
-                } 
+            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Gabinete biológico",
+                command: { cmd: "text", txt: "Gabinete biológico de Pablo Lapadula",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/pablo-lapadula#header", label: "", point: [97, 92]
+                }
             },
         }
     },
@@ -111,30 +113,30 @@ module.exports.ROOMS = {
             h8e24d9: { cmd: "enter", room: "hall", label: "Hall", point: [64, 96], enterPoint: [64, 72], obstacle: false },
         },
         things: {
-            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Vínculos empáticos", 
-                command: { cmd: "text", txt: "Vínculos empáticos\nde Demián Ferrari", 
-                    align: "center", lines: 2, url: "https://demiansferrari.wixsite.com/misitio/procesos", label: "", point: [25, 92] 
-                } 
+            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Vínculos empáticos",
+                command: { cmd: "text", txt: "Vínculos empáticos\nde Demián Ferrari",
+                    align: "center", lines: 2, url: "https://demiansferrari.wixsite.com/misitio/procesos", label: "", point: [25, 92]
+                }
             },
-            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Omphalos", 
-                command: { cmd: "text", txt: "Omphalos\nde Oliverio Duhalde", 
-                    align: "center", lines: 2, url: "https://www.oliverioduhalde.me/omphalos", label: "", point: [43, 80] 
-                } 
+            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Omphalos",
+                command: { cmd: "text", txt: "Omphalos\nde Oliverio Duhalde",
+                    align: "center", lines: 2, url: "https://www.oliverioduhalde.me/omphalos", label: "", point: [43, 80]
+                }
             },
-            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Biomateriales", 
-                command: { cmd: "text", txt: "Biomateriales\nde Verónica Bergottini y Ana Paula Hall", 
-                    align: "center", lines: 3, url: "https://bioarte-preview.netlify.app/artistas/biomateriales#header", label: "", point: [61, 92] 
-                } 
+            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Biomateriales",
+                command: { cmd: "text", txt: "Biomateriales\nde Verónica Bergottini y Ana Paula Hall",
+                    align: "center", lines: 3, url: "https://bioarte-preview.netlify.app/artistas/biomateriales#header", label: "", point: [61, 92]
+                }
             },
-            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "Biopigmentos", 
-                command: { cmd: "text", txt: "Biopigmentos\nde Luciana Paoletti", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/luciana-paoletti/#header", label: "", point: [79, 80] 
-                } 
+            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "Biopigmentos",
+                command: { cmd: "text", txt: "Biopigmentos\nde Luciana Paoletti",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/luciana-paoletti/#header", label: "", point: [79, 80]
+                }
             },
-            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Colonia", 
-                command: { cmd: "text", txt: "Colonia\nde Darío Sacco", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/dario-sacco#header", label: "", point: [97, 92] 
-                } 
+            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Colonia",
+                command: { cmd: "text", txt: "Colonia\nde Darío Sacco",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/dario-sacco#header", label: "", point: [97, 92]
+                }
             },
         }
     },
@@ -150,30 +152,30 @@ module.exports.ROOMS = {
             h8e24d9: { cmd: "enter", room: "hall", label: "Hall", point: [64, 96], enterPoint: [94, 75], obstacle: false },
         },
         things: {
-            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Con pan y con trabajo", 
-                command: { cmd: "text", txt: "Con pan y con trabajo\nde Daniel Alvarez Olmedo", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/daniel-alvarez-olmedo#header", label: "", point: [25, 92] 
-                } 
+            tubo1: { file: "tubo-1.png", frames: 2, frameDelay: 20, position: [22, 55], label: "Con pan y con trabajo",
+                command: { cmd: "text", txt: "Con pan y con trabajo\nde Daniel Alvarez Olmedo",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/daniel-alvarez-olmedo#header", label: "", point: [25, 92]
+                }
             },
-            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Namiki", 
-                command: { cmd: "text", txt: "Namiki\nde Muru", 
-                    align: "center", lines: 2, url: "https://namiki-universo-digital.netlify.app/", label: "", point: [43, 80] 
-                } 
+            tubo2: { file: "tubo-2.png", frames: 2, frameDelay: 20, position: [40, 48], label: "Namiki",
+                command: { cmd: "text", txt: "Namiki\nde Muru",
+                    align: "center", lines: 2, url: "https://namiki-universo-digital.netlify.app/", label: "", point: [43, 80]
+                }
             },
-            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Terrafonías", 
-                command: { cmd: "text", txt: "Terrafonías\nde Colectivo Electrobiota", 
-                    align: "center", lines: 2, url: "https://youtu.be/ISXYyUg8tHc", label: "", point: [61, 92] 
-                } 
+            tubo3: { file: "tubo-3.png", frames: 2, frameDelay: 20, position: [58, 55], label: "Terrafonías",
+                command: { cmd: "text", txt: "Terrafonías\nde Colectivo Electrobiota",
+                    align: "center", lines: 2, url: "https://youtu.be/ISXYyUg8tHc", label: "", point: [61, 92]
+                }
             },
-            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "El desafío de la vida", 
-                command: { cmd: "text", txt: "El desafío de la vida\nde Ariel Wilner", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/ariel-wilner#header", label: "", point: [79, 80] 
-                } 
+            tubo4: { file: "tubo-4.png", frames: 2, frameDelay: 20, position: [76, 48], label: "El desafío de la vida",
+                command: { cmd: "text", txt: "El desafío de la vida\nde Ariel Wilner",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/ariel-wilner#header", label: "", point: [79, 80]
+                }
             },
-            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Modelado Molecular", 
-                command: { cmd: "text", txt: "Modelado Molecular\nde Federico Coscio y Pablo Power", 
-                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/modelado-molecular#header", label: "", point: [97, 92] 
-                } 
+            tubo5: { file: "tubo-5.png", frames: 2, frameDelay: 20, position: [94, 55], label: "Modelado Molecular",
+                command: { cmd: "text", txt: "Modelado Molecular\nde Federico Coscio y Pablo Power",
+                    align: "center", lines: 2, url: "https://bioarte-preview.netlify.app/artistas/modelado-molecular#header", label: "", point: [97, 92]
+                }
             },
         }
     },
@@ -209,11 +211,11 @@ module.exports.ROOMS = {
             naranja: { file: "patio-formula-naranja.png", frames: 2, frameDelay: 30, position: [35, 53], label: "" },
             marron: { file: "patio-formula-marron.png", frames: 2, frameDelay: 30, position: [40, 50], label: "" },
             alga1: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [7, 38], label: "" },
-            alga2: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [13, 52], label: "" },
-            alga3: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [7, 72], label: "" },
+            alga2: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [13, 53], label: "" },
+            alga3: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [9, 72], label: "" },
             alga4: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [82, 12], label: "" },
-            alga5: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [119, 19], label: "" },
-            alga6: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [119, 72], label: "" },
+            alga5: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [117, 19], label: "" },
+            alga6: { file: "patio-alga.png", frames: 2, frameDelay: 30, position: [114, 73], label: "" },
         }
     },
 };
